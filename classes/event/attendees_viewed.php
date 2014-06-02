5e88e5b78d424bd13ec1dda55692d1deff5bc3c5
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-02 16:29:58 +0100

    Add skeletons

---
+++ classes/event/attendees_viewed.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_facetoface\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event Class
+ */
+class attendees_viewed extends \core\event\base
+{
+    /**
+     * Init method.
+     */
+    protected function init() {
+        global $CFG;
+
+        $this->data['objecttable'] = 'facetoface';
+        $this->data['crud'] = 'r';
+        $this->data[(floatval($CFG->release) <= 2.6) ? 'level' : 'edulevel'] = self::LEVEL_TEACHING;
+    }
+
+    /**
+     * Returns localised general event name.
+     * 
+     * @return string
+     */
+    public static function get_name() {
+        return "Facetoface Attendees Viewed";
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return 'User viewed attendees for Facetoface module \'' . $this->objectid . '\'.';
+    }
+
+    /**
+     * Returns relevant URL.
+     * 
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/facetoface/view.php', array('id' => $this->objectid));
+    }
+
+    /**
+     * Return the legacy event log data.
+     * 
+     * @return array
+     */
+    protected function get_legacy_logdata() {
+        return array($this->objectid, 'facetoface', 'view attendees', 'view.php?id=' . $this->objectid, '');
+    }
+}
---
+++ classes/event/session_updated.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_facetoface\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event Class
+ */
+class session_updated extends \core\event\base
+{
+    /**
+     * Init method.
+     */
+    protected function init() {
+        global $CFG;
+
+        $this->data['objecttable'] = 'facetoface';
+        $this->data['crud'] = 'u';
+        $this->data[(floatval($CFG->release) <= 2.6) ? 'level' : 'edulevel'] = self::LEVEL_PARTICIPATING;
+    }
+
+    /**
+     * Returns localised general event name.
+     * 
+     * @return string
+     */
+    public static function get_name() {
+        return "Facetoface Session Updated";
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return 'Updated session for Facetoface module \'' . $this->objectid . '\'.';
+    }
+
+    /**
+     * Returns relevant URL.
+     * 
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/facetoface/sessions.php', array('s' => $this->other['sessionid']));
+    }
+
+    /**
+     * Return the legacy event log data.
+     * 
+     * @return array
+     */
+    protected function get_legacy_logdata() {
+        return array($this->objectid, 'facetoface', 'session updated', 'sessions.php?s=' . $this->other['sessionid'], '');
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws \coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+
+        if (!isset($this->other['sessionid'])) {
+            throw new \coding_exception('The \'sessionid\' must be set.');
+        }
+    }
+}
