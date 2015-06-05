615404f1f6a6cd4a565ceaaacfeb554828aa955d
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-06-05 11:09:59 +0100

    Update version.php

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015021300;
+$plugin->version   = 2015060500;
 $plugin->requires  = 2014111000;  // Requires this Moodle version.
-$plugin->release   = '2.8.0 (Build: 2015021300)'; // User-friendly version number.
+$plugin->release   = '2.9.0 (Build: 2015060500)'; // User-friendly version number.
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
